56912d5553899429d2f63f31a3215f808b775afa
Marvin Besselsen <marvin@magmodules.eu>
2023-08-03 15:09:50 +0200

    [PLUG-27] Sending private key content with AJAJX request

--- Controller/Adminhtml/Credentials/Check.php
+++ Controller/Adminhtml/Credentials/Check.php
@@ -11,8 +11,13 @@ use Magento\Backend\App\Action;
 use Magento\Framework\App\Action\HttpPostActionInterface as HttpPostActionInterface;
 use Magento\Framework\Controller\Result\Json;
 use Magento\Framework\Controller\Result\JsonFactory;
+use Magento\Framework\Exception\FileSystemException;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Filesystem\DirectoryList;
+use Magento\Framework\Filesystem\Io\File;
 use TrueLayer\Connect\Api\Config\RepositoryInterface as ConfigRepository;
 use TrueLayer\Connect\Service\Api\GetClient;
+use TrueLayer\Interfaces\Client\ClientInterface;
 
 /**
  * Credential check controller to validate API data
@@ -20,6 +25,11 @@ use TrueLayer\Connect\Service\Api\GetClient;
 class Check extends Action implements HttpPostActionInterface
 {
 
+    private const PEM_UPLOAD_FILE = '/truelayer/temp/private_key.pem';
+    /**
+     * @var DirectoryList
+     */
+    private $directoryList;
     /**
      * @var GetClient
      */
@@ -32,6 +42,10 @@ class Check extends Action implements HttpPostActionInterface
      * @var ConfigRepository
      */
     private $configProvider;
+    /**
+     * @var File
+     */
+    private $file;
 
     /**
      * Check constructor.
@@ -40,16 +54,22 @@ class Check extends Action implements HttpPostActionInterface
      * @param JsonFactory $resultJsonFactory
      * @param GetClient $getClient
      * @param ConfigRepository $configProvider
+     * @param File $file
+     * @param DirectoryList $directoryList
      */
     public function __construct(
         Action\Context $context,
         JsonFactory $resultJsonFactory,
         GetClient $getClient,
-        ConfigRepository $configProvider
+        ConfigRepository $configProvider,
+        File $file,
+        DirectoryList $directoryList
     ) {
         $this->getClient = $getClient;
         $this->resultJson = $resultJsonFactory->create();
         $this->configProvider = $configProvider;
+        $this->file = $file;
+        $this->directoryList = $directoryList;
         parent::__construct($context);
     }
 
@@ -58,38 +78,10 @@ class Check extends Action implements HttpPostActionInterface
      */
     public function execute(): Json
     {
-        $config = $this->getCredentials();
-        if (!$config['credentials']['client_id'] || !$config['credentials']['client_secret']) {
-            return $this->resultJson->setData(
-                [
-                    'success' => true,
-                    'msg' => __('Set credentials first')
-                ]
-            );
-        }
-
         try {
-            $result = $this->getClient->execute(
-                (int)$config['store_id'],
-                ['credentials' => $config['credentials']]
-            );
-        } catch (\Exception $exception) {
+            $this->testCredentials()->getMerchantAccounts();
             return $this->resultJson->setData(
-                ['success' => false, 'msg' => $exception->getMessage()]
-            );
-        }
-        if (!$result) {
-            return $this->resultJson->setData(
-                ['success' => false, 'msg' => 'Credentials are not correct']
-            );
-        }
-        try {
-            $result->getMerchantAccounts();
-            return $this->resultJson->setData(
-                [
-                    'success' => true,
-                    'msg' => __('Credentials correct!')->render()
-                ]
+                ['success' => true, 'msg' => __('Credentials correct!')->render()]
             );
         } catch (\Exception $exception) {
             return $this->resultJson->setData(
@@ -99,7 +91,38 @@ class Check extends Action implements HttpPostActionInterface
     }
 
     /**
+     * @throws LocalizedException
+     * @throws FileSystemException
+     */
+    private function testCredentials(): ?ClientInterface
+    {
+        $config = $this->getCredentials();
+
+        if (!$config['credentials']['client_id']) {
+            throw new LocalizedException(__('No Client ID set!'));
+        }
+
+        if (!$config['credentials']['client_secret']) {
+            throw new LocalizedException(__('No Client Secret set!'));
+        }
+
+        $result = $this->getClient->execute(
+            (int)$config['store_id'],
+            ['credentials' => $config['credentials']]
+        );
+
+        $this->cleanSavedTemporaryPrivateKey();
+
+        if (!$result) {
+            throw new LocalizedException(__('Credentials are not correct.'));
+        }
+
+        return $result;
+    }
+
+    /**
      * @return array
+     * @throws FileSystemException
      */
     private function getCredentials(): array
     {
@@ -125,9 +148,44 @@ class Check extends Action implements HttpPostActionInterface
             'credentials' => [
                 'client_id' => $clientId,
                 'client_secret' => $clientSecret,
-                'private_key' => $configCredentials['private_key'],
+                'private_key' => $this->getPrivateKeyPath($configCredentials),
                 'key_id' => $keyId
             ]
         ];
+    }
+
+    /**
+     * @param array $configCredentials
+     * @return string
+     * @throws FileSystemException
+     */
+    private function getPrivateKeyPath(array $configCredentials): string
+    {
+        if ($privateKey = $this->getRequest()->getParam('private_key')) {
+            $path = $this->directoryList->getPath('var') . self::PEM_UPLOAD_FILE;
+            $fileInfo = $this->file->getPathInfo($path);
+
+            if (!$this->file->fileExists($fileInfo['dirname'])) {
+                $this->file->mkdir($fileInfo['dirname']);
+            }
+
+            $this->file->write($path, $privateKey);
+
+            return $path;
+        }
+
+        return $configCredentials['private_key'];
+    }
+
+    /**
+     * @return void
+     * @throws FileSystemException
+     */
+    private function cleanSavedTemporaryPrivateKey(): void
+    {
+        $path = $this->directoryList->getPath('var') . self::PEM_UPLOAD_FILE;
+        if ($this->file->fileExists($path)) {
+            $this->file->rm($path);
+        }
     }
 }
