e55dd2247b90de71f20aa4a1476d76391364f368
artyom-jaksov-tl <artyom.jaksov@truelayer.com>
2024-09-17 13:00:56 +0200

    PLUG-131: PHPStan (#26)
    * update phpstan config
    
    * fix batch of phpstan lvl 2 errors
    
    * fix batch of phpstan lvl 2 errors
    
    ---------
    
    Co-authored-by: Alexandru Lighezan <alex.lighezan@truelayer.com>

--- Service/Order/PaymentUpdate/PaymentFailedService.php
+++ Service/Order/PaymentUpdate/PaymentFailedService.php
@@ -62,6 +62,7 @@ class PaymentFailedService
      */
     private function cancelOrder(PaymentTransactionDataInterface $transaction, string $failureReason): void
     {
+        /** @var \Magento\Sales\Model\Order $order */
         $order = $this->orderRepository->get($transaction->getOrderId());
 
         if (!$order->isCanceled()) {
